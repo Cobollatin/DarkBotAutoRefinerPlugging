df9c8ab69cfa5575a26c00354c17f018486a69d5
Cobollatin <cobollatin@hotmail.com>
2025-04-03 17:00:19 -0500

    simplify

---
+++ src/main/java/com/rg4/darkbot/AutoRefinerConfig.java
@@ -0,0 +1,8 @@
+package com.rg4.darkbot;
+
+import eu.darkbot.api.config.annotations.Configuration;
+
+@Configuration("auto_refiner.config")
+public class AutoRefinerConfig {
+    public boolean Active = true;
+}
